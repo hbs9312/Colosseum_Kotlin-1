130b18c0caf619c5e0dcec01998053a06257a456
HwangboSeok <59465263+hbs9312@users.noreply.github.com>
2020-06-05 15:48:28 +0900

    커스텀 액션바 사용 옵션 부여 + 그 실제 표시 모양 XML 그리기

--- app/src/main/java/kr/co/tjoeun/colosseum_kotlin/BaseActivity.java
+++ app/src/main/java/kr/co/tjoeun/colosseum_kotlin/BaseActivity.java
@@ -2,6 +2,7 @@ package kr.co.tjoeun.colosseum_kotlin;
 
 import android.content.Context;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public abstract class BaseActivity extends AppCompatActivity {
@@ -11,4 +12,7 @@ public abstract class BaseActivity extends AppCompatActivity {
     public abstract void setupEvents();
     public abstract void setValues();
 
+    public void setCustomActionBar() {
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+    }
 }
