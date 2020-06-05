159395884d84f147ffae31c4d2278e5ddddcd2ed
HwangboSeok <59465263+hbs9312@users.noreply.github.com>
2020-06-05 11:05:32 +0900

    의견 상세보기 페이지 => 어떤 진영의 의견인지 정보 추가  - 의견별 선택진영 정보 추가된 것을 활용

--- app/src/main/java/kr/co/tjoeun/colosseum_kotlin/ViewReplyActivity.java
+++ app/src/main/java/kr/co/tjoeun/colosseum_kotlin/ViewReplyActivity.java
@@ -83,6 +83,7 @@ public class ViewReplyActivity extends BaseActivity {
     void setUiByReplyData() {
         binding.writerNickNameTxt.setText(mReplyData.getWriter().getNickName());
         binding.contentTxt.setText(mReplyData.getContent());
+        binding.sideTitleTxt.setText(mReplyData.getSelectedSide().getTitle());
     }
 }
 
